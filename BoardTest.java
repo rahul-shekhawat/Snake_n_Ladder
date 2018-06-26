07ba15ec529af3b76fd43a015050b571b49d1676
rahul4472 <40267157+rahul4472@users.noreply.github.com>
2018-06-26 13:19:44 +0530

    Update BoardTest.java

--- BoardTest.java
+++ BoardTest.java
@@ -1,11 +1,9 @@
 package com.qainfotech.tap.training.snl.api;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -73,6 +71,35 @@ public class BoardTest {
 		obj.deletePlayer(invalid);
 	}
 	
+	@Test(expectedExceptions={NoUserWithSuchUUIDException.class})
+	public void checking_successful_deletion() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException, NoUserWithSuchUUIDException
+	{
+		obj.registerPlayer("Player1");
+		obj.registerPlayer("Player2");
+		JSONObject data=new JSONObject();
+		data=obj.getData();
+		JSONArray array=new JSONArray();
+		array=data.getJSONArray("players");
+		JSONObject playertodelete = array.getJSONObject(0);
+		UUID uuid=(UUID) playertodelete.get("uuid");
+		obj.deletePlayer(uuid);
+		obj.deletePlayer(uuid);
+		
+	}
+	
+	@Test(expectedExceptions={PlayerExistsException.class})
+	public void checking_successful_registeration() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException
+	{
+		obj.registerPlayer("Player1");
+		JSONObject data=new JSONObject();
+		data=obj.getData();
+		JSONArray array=new JSONArray();
+		array=data.getJSONArray("players");
+		JSONObject playertoregister = array.getJSONObject(0);
+		String playername=playertoregister.getString("name");
+		obj.registerPlayer(playername);
+		
+	}
 	@Test(expectedExceptions={InvalidTurnException.class})
 	public void making_invalid_turn() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException, JSONException, InvalidTurnException
 	{
@@ -93,6 +120,32 @@ public class BoardTest {
 		}
 	}
 	
+	@Test
+	public void checkingrenderingofcorrectmessages() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException, JSONException, InvalidTurnException
+	{	
+		JSONArray array=new JSONArray();
+		obj.registerPlayer("Player1");
+		array=obj.registerPlayer("Player2");
+		
+		for(int i=0;i<2;i++) {
+			JSONObject player = array.getJSONObject(i);
+			String id = (String)player.get("uuid"); 
+			UUID playerUuid = UUID.fromString(id);
+			int currentPosition = (int)obj.data.getJSONArray("players").getJSONObject(i).get("position");
+			JSONObject response = obj.rollDice(playerUuid);
+			int diceValue = response.getInt("dice");
+			int type = (int) obj.data.getJSONArray("steps").getJSONObject(currentPosition+diceValue).get("type");
+			int newPosition = (int)obj.data.getJSONArray("players").getJSONObject(i).get("position");
+			if(type==0)
+				Assert.assertTrue(response.get("message").equals("Player moved to " + newPosition));
+			else if(type==1)
+				Assert.assertTrue(response.get("message").equals("Player was bit by a snake, moved back to " + newPosition));
+			else if(type==2)
+				Assert.assertTrue(response.get("message").equals("Player climbed a ladder, moved to " + newPosition));
+		}
+
+	}
+	
 	@BeforeMethod
 	public void launchwindow() throws FileNotFoundException, UnsupportedEncodingException, JSONException, IOException
 	{
@@ -102,6 +155,7 @@ public class BoardTest {
 		model=new BoardModel();
 		
 	}
+	
 	
 
 }
